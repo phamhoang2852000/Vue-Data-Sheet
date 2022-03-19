0ffeee8892f1f2d2fbd4c37d6a038f5a3f05dba9
phamhoang <phamhoanganh2852000@gmail.com>
2022-03-19 12:25:35 +0700

    data-sheet

---
+++ src/routes/routes.js
@@ -0,0 +1,56 @@
+import DashboardLayout from "@/pages/Layout/DashboardLayout.vue";
+
+import Dashboard from "@/pages/Dashboard.vue";
+import TableList from "@/pages/TableList.vue";
+import Typography from "@/pages/Typography.vue";
+import Icons from "@/pages/Icons.vue";
+import Notifications from "@/pages/Notifications.vue";
+import UpgradeToPRO from "@/pages/UpgradeToPRO.vue";
+import TableSheet from "@/pages/TableSheet.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: DashboardLayout,
+    redirect: "/dashboard",
+    children: [
+      {
+        path: "dashboard",
+        name: "Dashboard",
+        component: Dashboard,
+      },
+      {
+        path: "table",
+        name: "Table List",
+        component: TableList,
+      },
+      {
+        path: "table-sheet",
+        name: "Table Sheet",
+        component: TableSheet,
+      },
+      {
+        path: "typography",
+        name: "Typography",
+        component: Typography,
+      },
+      {
+        path: "icons",
+        name: "Icons",
+        component: Icons,
+      },
+      {
+        path: "notifications",
+        name: "Notifications",
+        component: Notifications,
+      },
+      {
+        path: "upgrade",
+        name: "Upgrade to PRO",
+        component: UpgradeToPRO,
+      },
+    ],
+  },
+];
+
+export default routes;
