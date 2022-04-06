21bbbed089b0a0ce58bfc3e11342f1d193790cc5
phamhoang2852000 <tho.nt@zinza.com.vn>
2022-04-06 22:14:08 +0700

    dropdown update

--- src/pages/DropDowns.vue
+++ src/pages/DropDowns.vue
@@ -1,67 +1,96 @@
 <template>
-  <div>
-    <div class="menu-container">
-      <nav>
-        <ul class="menu">
-          <li class="dropdown dropdown-1">
-            TranslateY
-            <ul class="dropdown_menu dropdown_menu-1">
-              <li class="dropdown_item-1">Item 1</li>
-              <li class="dropdown_item-2">Item 2</li>
-              <li class="dropdown_item-3">Item 3</li>
-              <li class="dropdown_item-4">Item 4</li>
-              <li class="dropdown_item-5">Item 5</li>
-            </ul>
-          </li>
-          <li class="dropdown dropdown-2">
-            RotateX
-            <ul class="dropdown_menu dropdown_menu-2">
-              <li class="dropdown_item-1">Item 1</li>
-              <li class="dropdown_item-2">Item 2</li>
-              <li class="dropdown_item-3">Item 3</li>
-              <li class="dropdown_item-4">Item 4</li>
-              <li class="dropdown_item-5">Item 5</li>
-            </ul>
-          </li>
-          <li class="dropdown dropdown-3">
-            TranslateZ
-            <ul class="dropdown_menu dropdown_menu-3">
-              <li class="dropdown_item-1">Item 1</li>
-              <li class="dropdown_item-2">Item 2</li>
-              <li class="dropdown_item-3">Item 3</li>
-              <li class="dropdown_item-4">Item 4</li>
-              <li class="dropdown_item-5">Item 5</li>
-            </ul>
-          </li>
-          <li class="dropdown dropdown-4">
-            Scale
-            <ul class="dropdown_menu dropdown_menu-4">
-              <li class="dropdown_item-1">Item 1</li>
-              <li class="dropdown_item-2">Item 2</li>
-              <li class="dropdown_item-3">Item 3</li>
-              <li class="dropdown_item-4">Item 4</li>
-              <li class="dropdown_item-5">Item 5</li>
-            </ul>
-          </li>
-          <li class="dropdown dropdown-5">
-            TranslateX
-            <ul class="dropdown_menu dropdown_menu-5">
-              <li class="dropdown_item-1">Item 1</li>
-              <li class="dropdown_item-2">Item 2</li>
-              <li class="dropdown_item-3">Item 3</li>
-              <li class="dropdown_item-4">Item 4</li>
-              <li class="dropdown_item-5">Item 5</li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
+  <div class="menu-container">
+    <div class="menu">
+      <div class="dropdown">
+        <table class="dropdown_menu dropdown_menu-4">
+          <thead>
+              <tr>
+                  <th>Row ID</th>
+                  <th>Sản phẩm</th>
+                  <th>Nhóm hàng</th>
+                  <th>Mã giảm</th>
+                  <th>Phân loại</th>
+                  <th>Kho</th>
+                  <th>Số lượng</th>
+                  <th>Đơn giá</th>
+                  <th>Giá tiền</th>
+                  <th>Khuyến mãi</th>
+              </tr>
+          </thead>
+          <tbody v-for="(product, index) in products" :key="product.id" :class="'dropdown_item-' + (index+1)" v-bind:style="{ display }">
+              <tr >
+                  <td>{{product.id}}</td>
+                  <td>{{product.product}}</td>
+                  <td>{{product.category}}</td>
+                  <td>{{product.discount_code}}</td>
+                  <td>{{product.classify}}</td>
+                  <td>{{product.total_product}}</td>
+                  <td>{{product.quantity}}</td>
+                  <td>{{product.price}}</td>
+                  <td>{{product.total_prince}}</td>
+                  <td>{{product.promotion}}</td>
+              </tr>
+          </tbody>
+        </table>
+       
+      </div>
+       <div class="button-group">
+          <md-button class="md-danger">Hủy</md-button>
+          <md-button @click="addProduct()" class="md-primary">Thêm sản phẩm</md-button>
+          <md-button class="md-success">Thanh Toán</md-button>
+        </div>
     </div>
+     
   </div>
 </template>
 
 <script>
+const axios = require('axios')
 export default {
   name: "dropdowns",
+  data() {
+    return {
+      products: [],
+      display:'none',
+      newdata: [
+        {
+          "id": "254",
+          "product": "Oxy12345",
+          "category": "Xtreme Admin",
+          "discount_code": "024680",
+          "classify": "B",
+          "total_product": 100,
+          "quantity": 2,
+          "price": 50000,
+          "total_prince": 100000,
+          "promotion": "Pending"
+        }
+      ]
+    };
+  },
+   mounted() {
+    axios.get('https://sheetdb.io/api/v1/fmnmuqs7ss0wp')
+      .then(response => {
+        console.log(response);
+        this.products = response.data;
+        this.display = 'table-header-group';
+      })
+      .error(error => {
+        console.log(error);
+      })
+  },
+   methods: {
+    addProduct() {
+      axios.post('https://sheetdb.io/api/v1/fmnmuqs7ss0wp', this.newdata)
+      .then(response => {
+        console.log(response);
+        location.reload();
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
+  }
 };
 </script>
 
@@ -69,174 +98,84 @@ export default {
 @import url("https://fonts.googleapis.com/css?family=Sen&display=swap");
 
 body {
-  background: #374e59;
-  color: #c06162;
-  font-family: "Sen";
-  margin-bottom: 200px;
-  height: 100%;
+ height: 100%
 }
-
-h1 {
-  font-size: 48px;
-  text-align: center;
-  margin-top: 30px;
-  margin-bottom: 40px;
+table {
+    border-collapse: separate;
+    border-spacing: 0px 10px;
+}
+tbody {
+    background: #FFFFFF
+}
+tr {
+    height: 40px;
+    margin-bottom: 10px;
+}
+th {
+    font-family: Montserrat-Medium;
+    font-size: 16px;
+    color: #fff;
+    line-height: 1.4;
+    text-transform: uppercase;
+    background-color: orange;
+}
+td {
+    font-family: Montserrat-Regular;
+    font-size: 16px;
+    color: #808080;
+    line-height: 1.4;
+    text-align: center;
+}
+.button-group {
+    float: right;
 }
 
 .menu-container {
-  margin-top: 20px;
-  h3 {
-    font-size: 24px;
-    margin-bottom: 10px;
-    text-align: center;
-  }
+  display: flex;
+  justify-content: center;
 }
+
 .menu-container:nth-child(3) {
   margin-top: 200px;
 }
 
-nav {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-}
-
 .menu {
-  display: flex;
   justify-content: center;
+  display: inline-table;
+  position: absolute;
+  top: 10%;
 
   .dropdown {
-    padding: 20px 40px;
-    display: flex;
     justify-content: center;
     align-items: center;
     color: white;
-    background: #c06162;
-    background: #34495e;
     position: relative;
     font-size: 18px;
     perspective: 1000px;
     z-index: 100;
   }
-  .dropdown:hover {
-    background: #2980b9;
-    cursor: pointer;
-  }
-  .dropdown:hover .dropdown_menu li {
-    display: block;
-  }
   .dropdown_menu {
-    position: absolute;
     top: 100%;
     left: 0;
-    width: 100%;
     perspective: 1000px;
     z-index: -1;
-    width: 100%;
     margin: 0px;
     padding: 0px;
-    li {
+    tbody {
       display: none;
-      color: white;
-      background-color: #34495e;
-      padding: 10px 20px;
       width: 100%;
       margin: 0px;
       font-size: 16px;
       opacity: 0;
-      &:hover {
-        background-color: #2980b9;
-      }
     }
   }
 
-  // Menu as a whole animated
-  .dropdown:hover .dropdown_menu--animated {
-    display: block;
-  }
-  .dropdown_menu--animated {
-    display: none;
-    li {
-      display: block;
-      opacity: 1;
-    }
-  }
   // Menu Items Animated invidually
-
-  .dropdown_menu-1 {
-    @for $num from 1 through 5 {
-      .dropdown_item-#{$num} {
-        transform-origin: top center;
-        animation: slideDown 300ms ($num * 60ms) ease-in-out forwards;
-      }
-    }
-  }
-
-  @keyframes slideDown {
-    0% {
-      opacity: 0;
-      transform: translateY(-60px);
-    }
-
-    100% {
-      opacity: 1;
-      transform: translateY(0);
-    }
-  }
-
-  .dropdown_menu-2 {
-    @for $num from 1 through 5 {
-      .dropdown_item-#{$num} {
-        transform-origin: top center;
-        animation: rotateX 300ms ($num * 60ms) ease-in-out forwards;
-      }
-    }
-  }
-
-  @keyframes rotateX {
-    0% {
-      opacity: 0;
-      transform: rotateX(-90deg);
-    }
-    50% {
-      transform: rotateX(-20deg);
-    }
-    100% {
-      opacity: 1;
-      transform: rotateX(0deg);
-    }
-  }
-
-  .dropdown_menu-3 {
-    @for $num from 1 through 5 {
-      .dropdown_item-#{$num} {
-        transform-origin: top center;
-        animation: rotateZ 300ms ($num * 60ms) ease-in-out forwards;
-      }
-    }
-  }
-
-  @keyframes rotateZ {
-    0% {
-      opacity: 0;
-      transform: translateZ(290px);
-    }
-
-    80% {
-      transform: translateZ(10px);
-    }
-
-    100% {
-      opacity: 1;
-      transform: translateZ(0);
-    }
-  }
-
   .dropdown_menu-4 {
-    @for $num from 1 through 5 {
+    @for $num from 1 through 100 {
       .dropdown_item-#{$num} {
         transform-origin: top center;
-        animation: scaleZ 300ms ($num * 60ms) ease-in-out forwards;
+        animation: scaleZ 300ms ($num * 100ms) ease-in-out forwards;
       }
     }
   }
@@ -254,56 +193,6 @@ nav {
     100% {
       opacity: 1;
       transform: scale(1);
-    }
-  }
-
-  .dropdown_menu-4 {
-    @for $num from 1 through 5 {
-      .dropdown_item-#{$num} {
-        transform-origin: top center;
-        animation: scaleZ 300ms ($num * 60ms) ease-in-out forwards;
-      }
-    }
-  }
-
-  @keyframes scaleZ {
-    0% {
-      opacity: 0;
-      transform: scale(0);
-    }
-
-    80% {
-      transform: scale(1.07);
-    }
-
-    100% {
-      opacity: 1;
-      transform: scale(1);
-    }
-  }
-
-  .dropdown_menu-5 {
-    @for $num from 1 through 5 {
-      .dropdown_item-#{$num} {
-        transform-origin: top center;
-        animation: translateX 300ms ($num * 60ms) ease-in-out forwards;
-      }
-    }
-  }
-
-  @keyframes translateX {
-    0% {
-      opacity: 0;
-      transform: translateX(60px);
-    }
-
-    80% {
-      transform: translateX(-5px);
-    }
-
-    100% {
-      opacity: 1;
-      transform: translateX(0px);
     }
   }
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,3 @@
-import DashboardLayout from "@/pages/Layout/DashboardLayout.vue";
-
-import Dashboard from "@/pages/Dashboard.vue";
-import TableList from "@/pages/TableList.vue";
-import Typography from "@/pages/Typography.vue";
-import Icons from "@/pages/Icons.vue";
-import Notifications from "@/pages/Notifications.vue";
-import UpgradeToPRO from "@/pages/UpgradeToPRO.vue";
-import TableSheet from "@/pages/TableSheet.vue";
 import DataSheet from "@/pages/DataSheet.vue";
 import Dropdowns from "@/pages/DropDowns.vue";
 
@@ -20,48 +11,6 @@ const routes = [
     path: "/drop-down",
     name: "DropDowns",
     component: Dropdowns,
-  },
-  {
-    path: "/",
-    component: DashboardLayout,
-    children: [
-      {
-        path: "dashboard",
-        name: "Dashboard",
-        component: Dashboard,
-      },
-      {
-        path: "table",
-        name: "Table List",
-        component: TableList,
-      },
-      {
-        path: "table-sheet",
-        name: "Table Sheet",
-        component: TableSheet,
-      },
-      
-      {
-        path: "typography",
-        name: "Typography",
-        component: Typography,
-      },
-      {
-        path: "icons",
-        name: "Icons",
-        component: Icons,
-      },
-      {
-        path: "notifications",
-        name: "Notifications",
-        component: Notifications,
-      },
-      {
-        path: "upgrade",
-        name: "Upgrade to PRO",
-        component: UpgradeToPRO,
-      },
-    ],
   },
 ];
 
